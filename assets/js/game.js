4123a55102e06ef70899499b713a33d86f5f916b
Brian Dunams <Brian Dunams>
2017-06-05 22:12:50 -0400

    version 1.0

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,8 +18,11 @@ var initialBid = 0;
 var initialBidder = "No one :-(";
 var highPrice = initialBid;
 var highBidder = initialBidder;
+var playerOneName = '';
+var playerTwoName = '';
 var playerOneGuess;
 var playerTwoGuess;
+var turn = 1;
 var ties;
 var wins;
 var losses;
@@ -44,8 +47,6 @@ connectedRef.on("value", function(snap) {
 
   // If they are connected..
   if (snap.val()) {
-    
-    $('#playerOneOptions').show();
 
     // Add user to the connections list.
     var con = connectionsRef.push(true);
@@ -60,44 +61,95 @@ connectionsRef.on("value", function(snap) {
 
   // Display the viewer count in the html.
   // The number of online users is the number of children in the connections list.
-  $("#watchers").html(snap.numChildren());
+  if(snap.numChildren() > 0){
+    $('#playerOneOptions').show();
+    $('#gameStatus').html('<p>Waiting on player two</p>');
+  }
+  if(snap.numChildren() === 2){
+    $('#playerTwoOptions').show();
+    $('#gameStatus').html('<p>Both players ready</p>');
+  }
+  
+  console.log(snap.child(1))
+  console.log(snap.numChildren())
+  
+});
+
+$('.options').on('click', function(){
+  $(this).addClass('btn-danger').removeClass('btn-primary');
+  turn++;
+})
+
+$('#add-player').on('click', function(event){
+  event.preventDefault();
+  
+  if(playerOneName === ''){
+    playerOneName = $('#name-input').val();
+    database.ref('players').push().update({
+      name: playerOneName
+    })
+  }
+  else if(playerTwoName === ''){
+    playerTwoName = $('name-input').val();
+    database.ref('players').push().update({
+      name: playerTwoName
+    })
+  }
+  
+  
+})
+
+// Create Firebase "watcher" Hint: .on("value")
+database.ref().on("value", function(snapshot) {
+  console.log(snapshot.val());
+  
+  database.ref().update({
+    turns: turn
+  })
+
+}, function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
 });
 
 
+
 // Game Rules
-if (playerOneGuess === playerTwoGuess){
-  ties+=1;
-  //alert('Its a tie');
-}
-else if(playerOneGuess === 'p'){
-  if(playerTwoGuess === 'r'){
-    wins+=1;
-    alert('paper beats rock');
+function results(){
+  
+  if (playerOneGuess === playerTwoGuess){
+    ties+=1;
+    //alert('Its a tie');
   }
-  else{
-    losses+=1;
-    alert('player losses');
+  else if(playerOneGuess === 'p'){
+    if(playerTwoGuess === 'r'){
+      wins+=1;
+      alert('paper beats rock');
+    }
+    else{
+      losses+=1;
+      alert('player losses');
+    }
   }
-}
 
-else if(playerOneGuess === 's'){
-  if(playerTwoGuess === 'p'){
-    wins+=1;
-    alert('scissors beat paper');
+  else if(playerOneGuess === 's'){
+    if(playerTwoGuess === 'p'){
+      wins+=1;
+      alert('scissors beat paper');
+    }
+    else{
+      losses+=1;
+      alert('player losses');
+    }
   }
-  else{
-    losses+=1;
-    alert('player losses');
-  }
-}
 
-else if(playerOneGuess === 'r'){
-  if(playerTwoGuess === 's'){
-    wins+=1;
-    alert('rock beats scissors');
-  }
-  else{
-    losses+=1;
-    alert('player losses');
+  else if(playerOneGuess === 'r'){
+    if(playerTwoGuess === 's'){
+      wins+=1;
+      alert('rock beats scissors');
+    }
+    else{
+      losses+=1;
+      alert('player losses');
+    }
   }
 }
